77348a4ecffd4549a2481f7b2d60a7067bf7e7ef
Robert Kinney <robertskinney@outlook.com>
2025-11-11 23:19:45 -0500

    feat: switch over to alternative auto-type

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,16 +2,13 @@ import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 import ComponentContainer from "./components/component-container";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { createGenerator } from "fumadocs-typescript";
-import { AutoTypeTable } from "fumadocs-typescript/ui";
-
-const generator = createGenerator();
+import { TypeTable } from 'fumadocs-ui/components/type-table';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    AutoTypeTable: (props) => (
-      <AutoTypeTable {...props} generator={generator} />
+    TypeTable: (props) => (
+      <TypeTable {...props} />
     ),
     ComponentContainer,
     SignedIn,
