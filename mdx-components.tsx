fabc5fc738fc0ea3f9948c51ea6a5cb01637d4c9
Robert Kinney <robertskinney@outlook.com>
2025-11-11 21:15:34 -0500

    test: updated auto type table

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -4,16 +4,14 @@ import ComponentContainer from "./components/component-container";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { createGenerator } from "fumadocs-typescript";
 import { AutoTypeTable } from "fumadocs-typescript/ui";
-import SignIn1 from "./registry/blocks/sign-in/sign-in-1";
-import SignUp1 from "./registry/blocks/sign-up/sign-up-1";
-import EmailManagement1 from "./registry/blocks/email-management/email-management-1";
-import SessionManagement1 from "./registry/blocks/session-management/session-management-1";
-import SocialAccounts1 from "./registry/blocks/social-accounts/social-accounts-1";
-import UserDropdown1 from "./registry/blocks/user-dropdown/user-dropdown-1";
-import UserDropdown2 from "./registry/blocks/user-dropdown/user-dropdown-2";
-import ProfileForm1 from "./registry/blocks/profile-form/profile-form-1";
 
-const generator = createGenerator();
+const generator = createGenerator({
+  tsconfigPath: './tsconfig.json',
+  // where to resolve relative paths (normally cwd)
+  basePath: './',
+  // disable file system cache
+  cache: false,
+});
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
@@ -24,14 +22,6 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     ComponentContainer,
     SignedIn,
     SignedOut,
-    SignIn1,
-    SignUp1,
-    EmailManagement1,
-    SessionManagement1,
-    SocialAccounts1,
-    UserDropdown1,
-    UserDropdown2,
-    ProfileForm1,
     ...components,
   };
 }
